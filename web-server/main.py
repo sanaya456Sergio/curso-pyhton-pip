67d6cb4780876b18c4f8842182a63977f809c6ea
Sergio Anaya <gerencia@itim-engineering.com>
2023-08-26 17:15:18 -0500

    mensaje

---
+++ web-server/main.py
@@ -0,0 +1,25 @@
+import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3,4]
+
+@app.get('/contact',response_class=HTMLResponse)
+def get_list():
+    #return {'name': 'Platzi'}
+    return """
+    <h1>Hola soy una página </h1>
+    <p>soy un parrafo</p>
+"""
+
+
+def run():
+    store.get_categories()
+
+
+if __name__ == '__main__':
+    run()
